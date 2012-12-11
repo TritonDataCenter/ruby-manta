b10e728550772378fc74ef84e1fe4134d8e11899
Marsell Kukuljevic <marsell@joyent.com>
2012-12-11 19:07:24 +1100

    Factor out prologue, epilogue in tests.

--- tests/test_manta.rb
+++ tests/test_manta.rb
@@ -9,21 +9,45 @@ class TestManta < MiniTest::Unit::TestCase
   @@user   = nil
 
   def setup
-    return if @@client
+    if ! @@client
+      host   = ENV['HOST']
+      key    = ENV['KEY' ]
+      @@user = ENV['USER']
 
-    host   = ENV['HOST']
-    key    = ENV['KEY' ]
-    @@user = ENV['USER']
+      unless host && key && @@user
+        $stderr.puts 'Require HOST, USER and KEY env variables to run tests.'
+        $stderr.puts 'E.g. USER=john KEY=~/.ssh/john HOST=https://manta.joyent.com ruby tests/test_manta.rb'
+        exit
+      end
 
-    unless host && key && @@user
-      puts 'Require HOST, USER and KEY to run tests.'
-      exit
+      priv_key_data = File.read(key)
+      http_client, fingerprint, priv_key = Manta.prepare(priv_key_data, :disable_ssl_verification => true)
+      @@client = Manta.new(http_client, host, @@user, fingerprint, priv_key)
+
+      @@test_dir_path = '/%s/stor/ruby-manta-test' % @@user
     end
 
-    priv_key_data = File.read(key)
-    http_client, fingerprint, priv_key = Manta.prepare(priv_key_data, :disable_ssl_verification => true)
+    teardown() 
 
-    @@client = Manta.new(http_client, host, @@user, fingerprint, priv_key)
+    result, _ = @@client.put_directory(@@test_dir_path)
+    assert_equal result, true
+  end
+
+
+
+  def teardown
+    listing, _ = @@client.list_directory(@@test_dir_path)
+    listing.each do |entry, _|
+      path = @@test_dir_path + '/' + entry['name']
+      if entry['type'] == 'directory'
+        @@client.delete_directory(path)
+      else
+        @@client.delete_object(path)
+      end
+    end
+
+    @@client.delete_directory(@@test_dir_path)
+  rescue Manta::ResourceNotFound
   end
 
 
@@ -61,30 +85,28 @@ class TestManta < MiniTest::Unit::TestCase
 
 
   def test_directories
-    test_dir_path = '/%s/stor/ruby-manta-test' % @@user
-
-    result, headers = @@client.put_directory(test_dir_path)
+    result, headers = @@client.put_directory(@@test_dir_path)
     assert_equal result, true
     assert headers.is_a? Hash
 
-    result, headers = @@client.put_directory(test_dir_path + '/dir1')
+    result, headers = @@client.put_directory(@@test_dir_path + '/dir1')
     assert_equal result, true
     assert headers.is_a? Hash
 
     # since idempotent
-    result, headers = @@client.put_directory(test_dir_path + '/dir1')
+    result, headers = @@client.put_directory(@@test_dir_path + '/dir1')
     assert_equal result, true
     assert headers.is_a? Hash
 
-    result, headers = @@client.put_object(test_dir_path + '/obj1', 'obj1-data')
+    result, headers = @@client.put_object(@@test_dir_path + '/obj1', 'obj1-data')
     assert_equal result, true
     assert headers.is_a? Hash
 
-    result, headers = @@client.put_object(test_dir_path + '/obj2', 'obj2-data')
+    result, headers = @@client.put_object(@@test_dir_path + '/obj2', 'obj2-data')
     assert_equal result, true
     assert headers.is_a? Hash
 
-    result, headers = @@client.list_directory(test_dir_path)
+    result, headers = @@client.list_directory(@@test_dir_path)
     assert headers.is_a? Hash
     assert_equal result.size, 3
 
@@ -102,50 +124,50 @@ class TestManta < MiniTest::Unit::TestCase
     assert_equal result[2]['size'], 9
     assert result[2]['mtime'].match(/^\d{4}-\d\d-\d\dT\d\d:\d\d:\d\dZ$/)
 
-    result, _ = @@client.list_directory(test_dir_path, :limit => 2)
+    result, _ = @@client.list_directory(@@test_dir_path, :limit => 2)
     assert_equal result.size, 2
     assert_equal result[0]['name'], 'dir1'
     assert_equal result[1]['name'], 'obj1'
 
-    result, _ = @@client.list_directory(test_dir_path, :limit => 1)
+    result, _ = @@client.list_directory(@@test_dir_path, :limit => 1)
     assert_equal result.size, 1
     assert_equal result[0]['name'], 'dir1'
 
-    result, _ = @@client.list_directory(test_dir_path, :limit  => 2,
+    result, _ = @@client.list_directory(@@test_dir_path, :limit  => 2,
                                                        :marker => 'obj1')
     assert_equal result.size, 2
     assert_equal result[0]['name'], 'obj1'
     assert_equal result[1]['name'], 'obj2'
 
-    result, headers = @@client.list_directory(test_dir_path, :head => true)
+    result, headers = @@client.list_directory(@@test_dir_path, :head => true)
     assert_equal result, true
     assert_equal headers['Result-Set-Size'], '3'
 
     begin
-      @@client.delete_directory(test_dir_path)
+      @@client.delete_directory(@@test_dir_path)
       assert false
     rescue Manta::DirectoryNotEmpty
     end
 
-    result, _ = @@client.delete_directory(test_dir_path + '/dir1')
+    result, _ = @@client.delete_directory(@@test_dir_path + '/dir1')
     assert_equal result, true
-    result, _ = @@client.delete_object(test_dir_path + '/obj1')
+    result, _ = @@client.delete_object(@@test_dir_path + '/obj1')
     assert_equal result, true
-    result, _ = @@client.delete_object(test_dir_path + '/obj2')
+    result, _ = @@client.delete_object(@@test_dir_path + '/obj2')
     assert_equal result, true
 
-    result, headers = @@client.delete_directory(test_dir_path)
+    result, headers = @@client.delete_directory(@@test_dir_path)
     assert_equal result, true
     assert headers.is_a? Hash
 
     begin
-      @@client.list_directory(test_dir_path + '/does-not-exist')
+      @@client.list_directory(@@test_dir_path + '/does-not-exist')
       assert false
     rescue Manta::ResourceNotFound
     end
 
     begin
-      @@client.put_directory(test_dir_path + '/dir1')
+      @@client.put_directory(@@test_dir_path + '/dir1')
       assert false
     rescue Manta::DirectoryDoesNotExist
     end
@@ -154,150 +176,106 @@ class TestManta < MiniTest::Unit::TestCase
 
 
   def test_objects
-    test_dir_path = '/%s/stor/ruby-manta-test' % @@user
-
-    result, _ = @@client.put_directory(test_dir_path)
-    assert_equal result, true
-
-    result, headers = @@client.put_object(test_dir_path + '/obj1', 'foo-data')
+    result, headers = @@client.put_object(@@test_dir_path + '/obj1', 'foo-data')
     assert_equal result, true
     assert headers.is_a? Hash
 
-    result, headers = @@client.get_object(test_dir_path + '/obj1')
+    result, headers = @@client.get_object(@@test_dir_path + '/obj1')
     assert_equal result, 'foo-data'
     assert_equal headers['Content-Type'], 'application/x-www-form-urlencoded'
 
-    result, headers = @@client.put_object(test_dir_path + '/obj1', 'bar-data',
+    result, headers = @@client.put_object(@@test_dir_path + '/obj1', 'bar-data',
                                           :content_type     => 'application/wacky',
                                           :durability_level => 3)
     assert_equal result, true
 
-    result, headers = @@client.get_object(test_dir_path + '/obj1')
+    result, headers = @@client.get_object(@@test_dir_path + '/obj1')
     assert_equal result, 'bar-data'
     assert_equal headers['Content-Type'], 'application/wacky'
 
-    result, headers = @@client.get_object(test_dir_path + '/obj1', :head => true)
+    result, headers = @@client.get_object(@@test_dir_path + '/obj1', :head => true)
     assert_equal result, true
     assert_equal headers['Content-Type'], 'application/wacky'
 
     begin
-      @@client.put_object(test_dir_path + '/obj1', 'bar-data',
+      @@client.put_object(@@test_dir_path + '/obj1', 'bar-data',
                           :durability_level => 999)
       assert false
     rescue Manta::InvalidDurabilityLevel
     end
 
     begin
-      @@client.get_object(test_dir_path + '/does-not-exist')
+      @@client.get_object(@@test_dir_path + '/does-not-exist')
       assert false
     rescue Manta::ResourceNotFound
     end
 
     begin
-      @@client.delete_object(test_dir_path + '/does-not-exist')
+      @@client.delete_object(@@test_dir_path + '/does-not-exist')
       assert false
     rescue Manta::ResourceNotFound
     end
 
-    result, headers = @@client.delete_object(test_dir_path + '/obj1')
+    result, headers = @@client.delete_object(@@test_dir_path + '/obj1')
     assert_equal result, true
     assert headers.is_a? Hash
-
-    result, _ = @@client.delete_directory(test_dir_path)
-    assert_equal result, true
   end
 
 
 
   def test_public
-    test_priv_dir_path = '/%s/stor/ruby-manta-test'   % @@user
+    result, _ = @@client.put_object(@@test_dir_path + '/obj1', 'foo-data')
+    assert_equal result, true
+
     test_pub_dir_path  = '/%s/public/ruby-manta-test' % @@user
-
-    result, _ = @@client.put_directory(test_priv_dir_path)
-    assert_equal result, true
-
-    result, _ = @@client.put_object(test_priv_dir_path + '/obj1', 'foo-data')
-    assert_equal result, true
+    host = ENV['HOST'].gsub('https', 'http')
 
     client = HTTPClient.new
     client.ssl_config.verify_mode = nil  # temp hack
-
-    host   = ENV['HOST'].gsub('https', 'http')
     result = client.get(host + test_pub_dir_path + '/obj1')
 # TODO: temporarily disabled due to bug in Manta service
 #    assert_equal result.body, 'foo-data'
-
-    result, headers = @@client.delete_object(test_priv_dir_path + '/obj1')
-    assert_equal result, true
-
-    result, _ = @@client.delete_directory(test_priv_dir_path)
-    assert_equal result, true
   end
 
 
 
   def test_signed_urls
-    test_dir_path = '/%s/stor/ruby-manta-test' % @@user
-
-    result, _ = @@client.put_directory(test_dir_path)
-    assert_equal result, true
-
-    result, _ = @@client.put_object(test_dir_path + '/obj1', 'foo-data')
+    result, _ = @@client.put_object(@@test_dir_path + '/obj1', 'foo-data')
     assert_equal result, true
 
     url = @@client.gen_signed_url(Time.now + 500000, :get,
-                                  test_dir_path + '/obj1')
+                                  @@test_dir_path + '/obj1')
 
     client = HTTPClient.new
     result = client.get('http://' + url)
     assert_equal result.body, 'foo-data'
-
-    result, headers = @@client.delete_object(test_dir_path + '/obj1')
-    assert_equal result, true
-
-    result, _ = @@client.delete_directory(test_dir_path)
-    assert_equal result, true
   end
 
 
 
   def test_links
-    test_dir_path = '/%s/stor/ruby-manta-test' % @@user
-
-    result, _ = @@client.put_directory(test_dir_path)
-    assert_equal result, true
-
     begin
-      @@client.put_link(test_dir_path + '/obj1', test_dir_path + '/obj2')
+      @@client.put_link(@@test_dir_path + '/obj1', @@test_dir_path + '/obj2')
       assert false
     rescue Manta::SourceObjectNotFound
     end
 
-    result, _ = @@client.put_object(test_dir_path + '/obj1', 'foo-data')
+    result, _ = @@client.put_object(@@test_dir_path + '/obj1', 'foo-data')
     assert_equal result, true
 
-    result, headers = @@client.put_link(test_dir_path + '/obj1',
-                                        test_dir_path + '/obj2')
+    result, headers = @@client.put_link(@@test_dir_path + '/obj1',
+                                        @@test_dir_path + '/obj2')
     assert_equal result, true
     assert headers.is_a? Hash
 
-    result, _ = @@client.put_object(test_dir_path + '/obj1', 'bar-data')
+    result, _ = @@client.put_object(@@test_dir_path + '/obj1', 'bar-data')
     assert_equal result, true
    
-    result, _ = @@client.get_object(test_dir_path + '/obj1')
+    result, _ = @@client.get_object(@@test_dir_path + '/obj1')
     assert_equal result, 'bar-data'
    
-    result, _ = @@client.get_object(test_dir_path + '/obj2')
+    result, _ = @@client.get_object(@@test_dir_path + '/obj2')
     assert_equal result, 'foo-data'
-
-    result, _ = @@client.delete_object(test_dir_path + '/obj1')
-    assert_equal result, true
-
-    result, _ = @@client.delete_object(test_dir_path + '/obj2')
-    assert_equal result, true
-
-    result, _ = @@client.delete_directory(test_dir_path)
-    assert_equal result, true
   end
 
 
