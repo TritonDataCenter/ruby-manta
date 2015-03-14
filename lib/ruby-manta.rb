e55ec11ec99de3a39b7825201f491632f7094667
Elijah Zupancic <elijah@zupancic.name>
2015-03-13 20:48:53 -0700

    Added alias so that MantaClient is compatible with the 1.x.x API. Reimported old test class in order to validate API compatibility. We may change the test to just verify the API and not the behavior in the future.

--- lib/ruby-manta.rb
+++ lib/ruby-manta.rb
@@ -2,3 +2,6 @@ module RubyManta
 end
 
 require_relative 'ruby-manta/manta_client'
+
+# Added API compatibility with 1.xx versions. This may be removed in the future
+MantaClient = RubyManta::MantaClient
